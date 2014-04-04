b1f9a0e7cfa3a3678738ee720a304e9ce27a5016
Pimm Hogeling <pimmhogeling@gmail.com>
2014-04-04 10:58:12 +0200

    There are static interpolation methods, allowing you to use them without creating instances.

--- library/src/org/ilumbo/hakama/interpolation/AccelerateInterpolator.java
+++ library/src/org/ilumbo/hakama/interpolation/AccelerateInterpolator.java
@@ -6,6 +6,16 @@ package org.ilumbo.hakama.interpolation;
  * Instances are stateless, hence can safely be re-used.
  */
 public final class AccelerateInterpolator implements ElapsedFactorInterpolator {
+	public static final class Static {
+		/**
+		 * Calculates and returns the interpolated elapsed fraction from the passed elapsed fraction. The passed elapsed
+		 * fraction must be between 0 and 1 (both including), where 0 represents the start of a glide and 1 represents the end.
+		 * The returned interpolated elapsed fraction is within the same range.
+		 */
+		public static final double interpolate(double input) {
+			return 1 - Math.cos((input * Math.PI) / 2);
+		}
+	}
 	@Override
 	public final double interpolate(double input) {
 		return 1 - Math.cos((input * Math.PI) / 2);
