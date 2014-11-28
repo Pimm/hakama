29d8f45d5720a025cad07334a2fb30c320dd2a9a
Pimm Hogeling <pimmhogeling@gmail.com>
2014-11-28 12:34:58 +0100

    Added deinterpolate implementations.

--- library/src/org/ilumbo/hakama/interpolation/AccelerateDecelerateInterpolator.java
+++ library/src/org/ilumbo/hakama/interpolation/AccelerateDecelerateInterpolator.java
@@ -29,6 +29,12 @@ package org.ilumbo.hakama.interpolation;
 public final class AccelerateDecelerateInterpolator implements ElapsedFactorInterpolator {
 	public static final class Static {
 		/**
+		 * Calculates and returns the input for which {@link #interpolate(double)} would return the passed output.
+		 */
+		public static final double deinterpolate(double output) {
+			return (2 * Math.asin(Math.sqrt(output))) / Math.PI;
+		}
+		/**
 		 * Calculates and returns the interpolated elapsed fraction from the passed elapsed fraction. The passed elapsed
 		 * fraction must be between 0 and 1 (both including), where 0 represents the start of a glide and 1 represents the end.
 		 * The returned interpolated elapsed fraction is within the same range.
@@ -36,6 +42,12 @@ public final class AccelerateDecelerateInterpolator implements ElapsedFactorInte
 		public static final double interpolate(double input) {
 			return (1 - Math.cos(input * Math.PI)) / 2;
 		}
+	}
+	/**
+	 * Calculates and returns the input for which {@link #interpolate(double)} would return the passed output.
+	 */
+	public final double deinterpolate(double output) {
+		return (2 * Math.asin(Math.sqrt(output))) / Math.PI;
 	}
 	@Override
 	public final double interpolate(double input) {
